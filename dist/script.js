0a229e05ee71ec8c9a56655fb6a0a184f279a864
Tink3rball3r <matthew_rittenhouse@hotmail.com>
2022-06-14 21:45:07 +0900

    auto hide nav

--- dist/script.js
+++ dist/script.js
@@ -2,6 +2,7 @@ const hamburgerBtn = document.querySelector('.hamburger-btn');
 const closeBtn = document.querySelector('.close-btn');
 const mobileNav = document.querySelector('.mobile-nav');
 const navLink = document.querySelectorAll('.nav-link');
+var prevScrollpos = window.pageYOffset;
 
 hamburgerBtn.addEventListener('click', function () {
 	mobileNav.classList.add('slide');
@@ -16,3 +17,13 @@ for (let i = 0; i < navLink.length; i++) {
 		mobileNav.classList.remove('slide');
 	});
 }
+// auto hide nav
+window.onscroll = function () {
+	var currentScrollPos = window.scrollY;
+	if (prevScrollpos > currentScrollPos) {
+		mobileNav.style.top = '0';
+	} else {
+		mobileNav.style.top = '-6rem';
+	}
+	prevScrollpos = currentScrollPos;
+};
