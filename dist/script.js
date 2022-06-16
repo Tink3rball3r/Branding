01df5264f04171cf7fc19cc0b9a50cd955cb3f58
Tink3rball3r <matthew_rittenhouse@hotmail.com>
2022-06-16 19:28:01 +0900

    fixed nav issues

--- dist/script.js
+++ dist/script.js
@@ -1,29 +1,32 @@
 const hamburgerBtn = document.querySelector('.hamburger-btn');
 const closeBtn = document.querySelector('.close-btn');
 const mobileNav = document.querySelector('.mobile-nav');
+const desktopNav = document.querySelector('.desktop-nav');
 const navLink = document.querySelectorAll('.nav-link');
 var prevScrollpos = window.pageYOffset;
 
+// opens the nav menu when clicked
 hamburgerBtn.addEventListener('click', function () {
 	mobileNav.classList.add('slide');
 });
-
+// closes the nav menu when clicked
 closeBtn.addEventListener('click', function () {
 	mobileNav.classList.remove('slide');
 });
-
+// closes the nav when selection is clicked
 for (let i = 0; i < navLink.length; i++) {
 	navLink[i].addEventListener('click', function () {
 		mobileNav.classList.remove('slide');
 	});
 }
+
 // auto hide nav
 window.onscroll = function () {
 	var currentScrollPos = window.scrollY;
 	if (prevScrollpos > currentScrollPos) {
-		mobileNav.style.top = '0';
+		desktopNav.style.top = '0';
 	} else {
-		mobileNav.style.top = '-6rem';
+		desktopNav.style.top = '-6rem';
 	}
 	prevScrollpos = currentScrollPos;
 };
